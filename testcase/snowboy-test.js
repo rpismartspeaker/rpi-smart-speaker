6c64b7b17abff3d0e2c501d598efc08ad3914d06
small_sunshine <jioo0224@naver.com>
2018-12-01 13:27:44 +0900

    testcase fix

--- testcase/snowboy-test.js
+++ testcase/snowboy-test.js
@@ -10,50 +10,38 @@ models.add({
   hotwords : 'OK Google'
 });
 
-const idleMode = () => {
-  const detector = new Detector({
-    resource: path.resolve(__dirname, '..', 'resources/snowboy/common.res'),
-    models: models,
-    audioGain: 2.0,
-    applyFrontend: true
-  });
+const detector = new Detector({
+  resource: path.resolve(__dirname, '..', 'resources/snowboy/common.res'),
+  models: models,
+  audioGain: 2.0,
+  applyFrontend: true
+})
 
-  detector.on('silence', function () {
-    console.log('silence');
-  });
+detector.on('silence', () => {
+  console.log('silence')
+})
 
-  detector.on('sound', function (buffer) {
-    // <buffer> contains the last chunk of the audio that triggers the "sound"
-    // event. It could be written to a wav stream.
-    console.log('sound');
-  });
+detector.on('sound', (buffer) => {
+  // <buffer> contains the last chunk of the audio that triggers the "sound"
+  // event. It could be written to a wav stream.
+  console.log('sound')
+});
 
-  detector.on('error', function () {
-    console.log('error');
-  });
+detector.on('error', () => {
+  console.log('error')
+});
 
-  detector.on('hotword', function (index, hotword, buffer) {
-    // <buffer> contains the last chunk of the audio that triggers the "hotword"
-    // event. It could be written to a wav stream. You will have to use it
-    // together with the <buffer> in the "sound" event if you want to get audio
-    // data after the hotword.
-    console.log(buffer);
-    console.log('hotword', index, hotword);
-  });
+detector.on('hotword', (index, hotword, buffer) => {
+  // <buffer> contains the last chunk of the audio that triggers the "hotword"
+  // event. It could be written to a wav stream. You will have to use it
+  // together with the <buffer> in the "sound" event if you want to get audio
+  // data after the hotword.
+  console.log(buffer)
+  console.log('hotword', index, hotword)
+});
 
-  /* const mic = record.start({
-    threshold: 0,
-    verbose: true,
-    recordProgram: 'arecord'
-  });
-
-  mic.pipe(detector);*/
-
-  record.start({
-    threshold: 0,
-    verbose: true,
-    recordProgram: 'arecord'
-  }).pipe(detector)
-}
-
-idleMode()
+const mic = record.start({
+  threshold: 0,
+  verbose: true,
+  recordProgram: 'arecord'
+}).pipe(detector)
