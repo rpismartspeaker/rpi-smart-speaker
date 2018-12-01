09f45ec0587d54745e4771e2db3c4eab58fc6af5
small_sunshine <jioo0224@naver.com>
2018-12-01 14:38:51 +0900

    테스트케이스 재수정

--- testcase/snowboy-test.js
+++ testcase/snowboy-test.js
@@ -46,10 +46,15 @@ detector.on('hotword', (index, hotword, buffer) => {
 });
 
 const mic = record.start({
-  sampleRate    : 16000,      // audio sample rate
-  threshold     : 0,          // silence threshold (rec only)
-  verbose       : true,       // log info to the console
-  recordProgram : 'sox',  // Defaults to 'arecord' - also supports 'rec' and 'sox'
+  sampleRate    : 16000,  // audio sample rate
+  channels      : 1,      // number of channels
+  threshold     : 0.5,    // silence threshold (rec only)
+  thresholdStart: null,   // silence threshold to start recording, overrides threshold (rec only)
+  thresholdEnd  : null,   // silence threshold to end recording, overrides threshold (rec only)
+  silence       : '1.0',  // seconds of silence before ending
+  verbose       : false,  // log info to the console
+  recordProgram : 'arecord',  // Defaults to 'rec' - also supports 'arecord' and 'sox'
+  device        : null,   // recording device (e.g.: 'plughw:1')
 })
 .on('error', error => {
   console.log(error)
